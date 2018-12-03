dfb700b164f849b47d65f04c4c23d2e6eb8841a7
Arnas Petruškevičius <arnas@tesonet.lt>
2018-12-03 21:24:57 +0200

    Finish day 3

---
+++ lib/day3/exercise2.ex
@@ -0,0 +1,32 @@
+defmodule AdventOfCode.Day3.Exercise2 do
+  import AdventOfCode.Day3.Exercise1
+
+  def solve do
+    AdventOfCode.Utils.FileReader.get_day_strings(3)
+    |> Enum.map(&parse_claim/1)
+    |> Enum.map(&convert_to_coordinates/1)
+    |> Enum.reduce(%{}, fn x, acc -> fill_grid_with_square(acc, x) end)
+    |> get_not_overlapping_ids
+    |> Enum.at(0)
+  end
+
+  def get_not_overlapping_ids(grid) do
+    MapSet.difference(get_all_ids(grid), get_overlapping_ids(grid))
+  end
+
+  def get_all_ids(grid) do
+    grid
+    |> Enum.unzip
+    |> Kernel.elem(1)
+    |> Enum.concat
+    |> MapSet.new
+  end
+
+  def get_overlapping_ids(grid) do
+    grid
+    |> Enum.reduce(MapSet.new, fn
+      {_, [_]}, acc -> acc
+      {_, list}, acc -> for x <- list, into: acc, do: x
+    end)
+  end
+end
---
+++ test/day3/exercise2_test.exs
@@ -0,0 +1,9 @@
+defmodule AdventOfCode.Day3.Exercise2Test do
+  use ExUnit.Case
+
+  import AdventOfCode.Day3.Exercise2
+
+  test "gets non overlapping ids" do
+    assert get_not_overlapping_ids(%{{1,1} => [2,3],{0,0} => [1], {2,2} => [5], {4,4} => [1,2,3]}) == MapSet.new([5])
+  end
+end
